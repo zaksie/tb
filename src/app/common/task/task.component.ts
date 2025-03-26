4311a10f0932bd214dc29939207cc4f94ae9a754
Zak Goichman <zak.goichman@gmail.com>
2025-03-26 15:23:00 +0200

    .

--- src/app/common/task/task.component.ts
+++ src/app/common/task/task.component.ts
@@ -9,13 +9,14 @@ import {GenericTask} from "../../models/clan-data.model";
 })
 export class TaskComponent {
   @Input() task!: GenericTask
-
+  readonly MIN = 0.7
+  readonly MAX = 1
   get isOpenGoal(): boolean {
     return this.task.goal <= 0
   }
 
   fitTitle(title: string) {
-    const res = Math.min(1, 1 - (title.length - 10) * 0.111)
+    const res = Math.max(this.MIN, Math.min(this.MAX, 1 - (title.length - 10) * 0.111))
     return res + 'rem'
   }
 }
