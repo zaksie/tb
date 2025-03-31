600eb9e1c8873a96d0a87a473117f94f9a63b95a
Zak Goichman <zak.goichman@gmail.com>
2025-03-31 15:07:56 +0300

    .

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {ServerModule} from '@angular/platform-server';
 import {AppComponent} from './app.component';
-import {AppModule, authorizationParams} from './app.module';
-import {AuthHttpInterceptor, AuthService, provideAuth0} from "@auth0/auth0-angular";
+import {AppModule} from './app.module';
+import {AuthHttpInterceptor, AuthService} from "@auth0/auth0-angular";
 import {of} from "rxjs";
 import {FlexLayoutServerModule} from "@angular/flex-layout/server";
 import {Socket} from "ngx-socket-io";
-import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
-import {environment} from "../environments/environment";
 
 @NgModule({
   imports: [
